c4c2cb59cdb73d7945fdf53341070d22f0a8a337
Jhans-15 <72289225+JFrancodev12@users.noreply.github.com>
2025-08-29 18:54:59 -0500

    fx:download

--- src/features/components/Install.js
+++ src/features/components/Install.js
@@ -1,21 +1,16 @@
 import { useEffect, useState } from "react";
+import './styles/install.css'
 
 export default function Install () {
 
-    const [ deferredPrompt, setDeferredPrompt ] = useState(null);
-    const [ visible, setVisible ] = useState(false);
+    const [deferredPrompt, setDeferredPrompt] = useState(null);
+    const [visible, setVisible] = useState(true);
 
     useEffect(() => {
-
-        const isStandalone = window.matchMedia("(display-mode: standalone)").matches || window.navigator.standalone === true;
-
-        if (isStandalone) return; // No mostrar si ya está instalada
-
-        // Capturamos el evento que dispara el navegador
         const handleBeforeInstallPrompt = (e) => {
             e.preventDefault();
             setDeferredPrompt(e);
-            setVisible(true);
+            setVisible(false)
         };
 
         window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
@@ -23,35 +18,34 @@ export default function Install () {
         return () => {
             window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
         };
-
-    }, [])
+    }, []);
 
     const handleInstall = async () => {
-        if (!deferredPrompt) return;
-        deferredPrompt.prompt(); // Mostrar modal nativo
-        const { outcome } = await deferredPrompt.userChoice;
-        if (outcome === "accepted") {
-            console.log("✅ Usuario aceptó instalar");
-        } else {
-            console.log("❌ Usuario rechazó instalar");
+        if (!deferredPrompt) {
+            alert("❌ No se puede instalar la app en este dispositivo o ya está instalada.");
+            return;
         }
+
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+
+        console.log(outcome === "accepted" ? "✅ Instalado" : "❌ Rechazado");
         setDeferredPrompt(null);
-        setVisible(false);
     };
 
-    if (!visible) return null;
+    if (!deferredPrompt || !visible) return null;
 
     return (
 
-        <div>
-
-            <div>
-                <h4>Descarga la app</h4>
-                <p>Descarga y disfruta de la mejor manera de conocer Jauja</p>
+        <section className="__section_install">
+            <div className={`__baner_install`}>
+                <div className="__col">
+                    <h3>Descarga la app</h3>
+                    <p>Descarga y disfruta de la mejor manera de conocer Jauja</p>
+                </div>
+                <button className="__btn_install" onClick={handleInstall}>Descargar</button>
             </div>
-            <button onClick={handleInstall}>Descargar</button>
-
-        </div>
+        </section>
 
     )
 
