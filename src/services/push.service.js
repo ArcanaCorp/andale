656f2679cf563a2395b46a5481b18453b3e2a16e
Jhans-15 <72289225+JFrancodev12@users.noreply.github.com>
2026-01-21 15:50:00 -0500

    fix:log_user

--- src/services/push.service.js
+++ src/services/push.service.js
@@ -1,17 +1,15 @@
-import Cookies from 'js-cookie'
 import { subscribeToPush } from '../utils/pushsubscribe';
 import { REACT_APP_API } from '../config';
 
 export const servicePushSubscribe = async () => {
-    const token = Cookies.get('c_user');
     const annonId = localStorage.getItem('anon_user_id')
-    if (!token && !annonId) return console.log({ok: false, error: 'No se encontraron los datos necesarios'});
+    if (!annonId) return;
     const subscription = await subscribeToPush();
     await fetch(`${REACT_APP_API}/notifications/subscribe`, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${token || annonId}`
+            Authorization: `Bearer ${annonId}`
         },
         body: JSON.stringify({ subscription })
     })
