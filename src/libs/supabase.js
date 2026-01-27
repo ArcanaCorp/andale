68eda6f2a19b765ea4b25e814fae4a2bdab7f69a
Jhans-15 <72289225+JFrancodev12@users.noreply.github.com>
2026-01-27 02:08:18 -0500

    fix:upgrade:supabase

--- src/libs/supabase.js
+++ src/libs/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-import { SUPABASE_KEY, SUPABASE_URL } from "@/config";
+//import { SUPABASE_KEY, SUPABASE_URL } from "@/config";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY)
