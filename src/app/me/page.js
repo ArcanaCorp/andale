a32715ff59f4c3abf3f3d52bc40b60e0eff09cbc
Jhans-15 <72289225+JFrancodev12@users.noreply.github.com>
2026-07-26 17:29:56 -0500

    fix:auth

--- src/app/me/page.js
+++ src/app/me/page.js
@@ -2,13 +2,37 @@
 import Avatar from "@/components/ui/Avatars/Avatar";
 import ButtonIcon from "@/components/ui/Buttons/ButtonIcon";
 import { useAuth } from "@/context/AuthContext";
+import { db } from "@/libs/supabase";
 import { IconBell, IconBook, IconChevronLeft, IconChevronRight, IconHeart, IconInfoCircle, IconLogout, IconMapPin, IconShoppingBag, IconShoppingBagPlus, IconUser } from "@tabler/icons-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Page () {
     
     const router = useRouter();
     const { user } = useAuth();
+
+    const [loading, setLoading] = useState(false);
+
+    const handleLogout = async () => {
+        try {
+            setLoading(true);
+
+            const { error } = await db.auth.signOut({
+                scope: 'local'
+            });
+
+            if (error) {
+                throw error;
+            }
+
+            router.replace('/');
+            router.refresh();
+        } catch (error) {
+            console.error('Error al cerrar sesión:', error);
+            setLoading(false);
+        }
+    };
     
     return (
         <>
@@ -58,8 +82,8 @@ export default function Page () {
                             <div className="flex gap-sm items-center text-sm"><IconShoppingBagPlus/> Registrar mi negocio</div>
                             <IconChevronRight/>
                         </li>
-                        <li className="w-full flex items-center justify-between py-sm pointer">
-                            <div className="flex gap-sm items-center text-sm"><IconLogout/> Cerrar Sesión</div>
+                        <li className="w-full flex items-center justify-between py-sm pointer" onClick={handleLogout}>
+                            <div className="flex gap-sm items-center text-sm"><IconLogout/> {loading ? 'Cerrando...' : 'Cerrar Sesión'}</div>
                         </li>
                     </ul>
                 </div>
