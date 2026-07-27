0406386873faf3b115c97197e5b2c2d601bf6c0d
Jhans-15 <72289225+JFrancodev12@users.noreply.github.com>
2026-07-27 16:55:01 -0500

    fix:btn_notify

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import { useDB } from "@/context/DBContext";
 import Link from "next/link";
 import Login from "./auth/login";
 import Loading from "@/components/views/Loading";
+import ButtonCustomerNotifications from "@/components/ui/Buttons/ButtonCustomerNotifications";
 
 export default function Page() {
     const { places, business } = useDB();
@@ -22,6 +23,7 @@ export default function Page() {
         <>
             <Header />
             <main className="w-full h scroll-y py-md" style={{"--h": "calc(100dvh - 190px)"}}>
+                <ButtonCustomerNotifications user={user} />
                 <section>
                     <div className="flex w-full items-center justify-between px-md">
                         <h3 className="text-md text-semibold">
